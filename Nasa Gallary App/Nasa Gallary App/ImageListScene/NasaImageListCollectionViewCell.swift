96d4916a6fe46d2ed5da2e73e03e68bc5a9f01e0
Shailesh Parkhi <Shaileshnparkhi@gmail.com>
2022-09-01 16:12:40 +0530

    Added view for image list
    Added viewcontroller, presenter, interactor.model and router for image list.
    
    created basic UI for image details
    
    added json file for data.

---
+++ Nasa Gallary App/Nasa Gallary App/ImageListScene/NasaImageListCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  NasaImageListCollectionViewCell.swift
+//  Nasa Gallary App
+//
+//  Created by ADMIN on 01/09/22.
+//
+
+import UIKit
+
+class NasaImageListCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    
+}
